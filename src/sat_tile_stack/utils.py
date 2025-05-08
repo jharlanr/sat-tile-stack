6468e6b78bcb68e739a58e159a211c99cc253d56
jharlanr <joshuahrines@gmail.com>
2025-05-08 21:41:02 +0000

    updated utils.py

--- src/sat_tile_stack/utils.py
+++ src/sat_tile_stack/utils.py
@@ -4,7 +4,11 @@ import numpy as np
 
 ## FUNCTION FOR NORMALIZING IMAGERY BEFORE STACKING
 def combo_scaler(x, range_max=1):
-    median_x = np.nanmedian(x)
-    iqr_x = np.nanpercentile(x,75) - np.nanpercentile(x,25)
-    robust_x = ((x-median_x)/iqr_x)
-    return ((robust_x - np.nanmin(robust_x)) / (np.nanmax(robust_x) - np.nanmin(robust_x))) * range_max
+    x = np.asarray(x)
+    with np.errstate(all="ignore"):
+        median_x = np.nanmedian(x)
+        iqr = np.nanpercentile(x, 75) - np.nanpercentile(x, 25)
+        robust = (x - median_x) / iqr
+        mn, mx = np.nanmin(robust), np.nanmax(robust)
+        scaled = (robust - mn) / (mx - mn) * range_max
+    return scaled
