0178566ab9a5a89e067f044acb86ee833d9af41f
jharlanr <joshuahrines@gmail.com>
2025-05-12 18:51:51 +0000

    fixed typo

--- src/sat_tile_stack/__init__.py
+++ src/sat_tile_stack/__init__.py
@@ -4,11 +4,15 @@ __version__="0.1.0"
 
 from .stack import sattile_stack
 from .bounds import sat_mask_array, bounds_latlon_around, best_crs_for_point
+from .io import scrub_attrs, sanitise_dataset, coerce_attrs_to_json_safe, write_netcdf_from_da
+from .visualize import timestack_to_movie
 from .utils import combo_scaler
 
 __all__ = [
-    "sattile_stack"
+    "sattile_stack",
     "sat_mask_array", "bounds_latlon_around", "best_crs_for_point",
+    "scrub_attrs", "sanitise_dataset", "coerce_attrs_to_json_safe", "write_netcdf_from_da",
+    "timestack_to_movie",
     "combo_scaler",
 ]
 
